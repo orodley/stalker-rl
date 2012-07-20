31cae8f5fa40d3a4bfeb9a9cffca138fa8fa58ee
Strigoides <Strigoides@gmail.com>
2012-07-20 19:57:32 +1200

    Optimized map generation code, camera no centers a set distance ahead of player

--- constant.py
+++ constant.py
@@ -10,6 +10,11 @@ COLOR_VARIATION = 15     # r, g, and b variation to apply for tiles
 
 MEMORY_TINT = 0.2        # r, g, and b tint for previously, but not currently seen tiles
 
-FOV_ANGLE = radians(120) # How wide can the player see, in radians
+FOV_ANGLE = radians(120) # How wide the player can see, in radians
 
 MAX_CONDITION = 10000    # Perfect condition for a weapon
+
+CAMERA_DISTANCE = 40     # How far ahead of the player to center the camera
+
+INENTORY_SIZE = (9, 10)  # Size of the inventory grid for the player
+SQUARE_SIZE = 5          # How large each square in the grid is
